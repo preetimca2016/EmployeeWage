aaf8397003e8935f8b7432442358eb015559a856
“preetimca2016” <preetimca2016@gmail.com>
2022-08-08 22:40:36 +0530

    msg

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            UC1EmployeePresentAbsent employee = new UC1EmployeePresentAbsent();
+            employee.PresentAbsent();
+
+            UC2DailyWage wage = new UC2DailyWage();
+            wage.DailyWage();
+
+            UC3PartTimeWage PartTimeWage = new UC3PartTimeWage();
+            PartTimeWage.EmployeePartTimeWage();
+
+            UC4SwitchStatement switchwage = new UC4SwitchStatement();   
+            switchwage.SwitchDailyWage();
+
+            Console.WriteLine("Total Monthly wage is :- ");
+            UC5MonthWage monthWage = new UC5MonthWage();
+            monthWage.MonthlyWage();
+
+        }
+    }
+}
---
+++ EmployeeWage/UC1EmployeePresentAbsent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class UC1EmployeePresentAbsent
+    {
+        public void PresentAbsent()
+        {
+            Random EmpCheck = new Random();
+            int Check = EmpCheck.Next(0, 2);
+            int EmployeePresent = 1;
+            if(Check == EmployeePresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+        
+    }
+}
